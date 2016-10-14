a51b7f6e5537d22c7385c288c32a35a2ddeb8f09
user <user@FE07>
2016-10-14 10:02:06 +0800

    Ver1.5.1 add require

--- src/fcu/iecs/oop/java/StringComparator.java
+++ src/fcu/iecs/oop/java/StringComparator.java
@@ -7,15 +7,22 @@ public class StringComparator
 	public static void main(String[] args)
 	{
 		Scanner keyboard = new Scanner(System.in);
-		String com1,com2;
-		System.out.print("Enter a string 1:");
-		com1=keyboard.next();
-		System.out.print("Enter a string 2:");
-		com2=keyboard.next();
-		if(com1.compareToIgnoreCase(com2)==0)
-		System.out.print("The two strings are the same.");
-		else
-		System.out.print("The two strings are not the same.");
+		String com1, com2;
+		while (true) 
+		{
+			System.out.print("Enter a string 1:");
+			com1 = keyboard.next();
+			System.out.print("Enter a string 2:");
+			com2 = keyboard.next();
+			if (com1.compareToIgnoreCase(com2) == 0)
+			{
+				System.out.print("The two strings are the same.");
+				break;	
+			}
+			else
+				System.out.print("The two strings are not the same.\n");
+			
+		}
 		keyboard.close();
-	}	
+	}
 }
